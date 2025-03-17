9099cc0c8078b91c0ef568a1000588fcfa08b8d3
Ertays <sirenertays@gmail.com>
2025-03-17 19:28:48 +0100

    Add files via upload

---
+++ 123121/123121.cpp
@@ -0,0 +1,23 @@
+﻿//Завдання 2
+//Використовуючи покажчики та оператор розіменування, визначити знак числа, введеного з клавіатури.
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int number;
+
+	cout << "Your number: ";
+	cin >> number;
+
+	int* ptrN = &number;
+
+	if (*ptrN > 0) {
+		cout << "This number is passiv!" << endl;
+	}
+	else {
+		cout << "This number is negative!" << endl;
+	}
+}
